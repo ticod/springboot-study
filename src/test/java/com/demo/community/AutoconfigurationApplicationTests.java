e7149fee6d8d41e0c23f6bbb32d31b92b806e917
tico <lours2021@gmail.com>
2022-04-14 14:01:53 +0900

    필요 없는 import 제거

--- src/test/java/com/demo/community/AutoconfigurationApplicationTests.java
+++ src/test/java/com/demo/community/AutoconfigurationApplicationTests.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.List;
