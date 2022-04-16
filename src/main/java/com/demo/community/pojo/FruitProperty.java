2dac09cfdbc5982d634286a47c43248552e8468f
tico <lours2021@gmail.com>
2022-04-16 22:54:00 +0900

    2022.04.16 pojo type 매핑
    Map -> POJO

--- src/main/java/com/demo/community/pojo/FruitProperty.java
+++ src/main/java/com/demo/community/pojo/FruitProperty.java
@@ -5,13 +5,12 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
-import java.util.Map;
 
 @Data
 @Component
 @ConfigurationProperties("fruit")
 public class FruitProperty {
 
-    private List<Map> list;
+    private List<Fruit> list;
 
 }
