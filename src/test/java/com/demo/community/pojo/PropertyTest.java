3b072475c4aed3a12dcf1a44bc0287f36011fdff
tico <lours2021@gmail.com>
2022-04-16 22:52:01 +0900

    2022.04.16 pojo type 매핑

---
+++ src/test/java/com/demo/community/pojo/PropertyTest.java
@@ -0,0 +1,36 @@
+package com.demo.community.pojo;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.is;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class PropertyTest {
+
+    @Autowired
+    FruitProperty fruitProperty;
+
+    @Test
+    public void test() {
+        List<Map> fruitData = fruitProperty.getList();
+
+        assertThat(fruitData.get(0).get("name"), is("banana"));
+        assertThat(fruitData.get(0).get("color"), is("yellow"));
+
+        assertThat(fruitData.get(1).get("name"), is("apple"));
+        assertThat(fruitData.get(1).get("color"), is("red"));
+
+        assertThat(fruitData.get(2).get("name"), is("water melon"));
+        assertThat(fruitData.get(2).get("color"), is("green"));
+    }
+
+}
